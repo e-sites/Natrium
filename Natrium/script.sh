f051ba5c5d31bf6008051313f585eab4b842b01b
Bas van Kuijck <bas@e-sites.nl>
2016-05-12 08:50:20 +0200

    ./.__environment.lgo

--- Natrium/script.sh
+++ Natrium/script.sh
@@ -2,4 +2,4 @@
 # Shell script to forward xcode build settings to the ruby script
 export PATH
 
-ruby "./environmentbuild.rb" --project_dir "${PROJECT_DIR}" --infoplist_file "${INFOPLIST_FILE}" --configuration "${CONFIGURATION}" --environment $* > "${MYPATH}/.__environment.log"
+ruby "./environmentbuild.rb" --project_dir "${PROJECT_DIR}" --infoplist_file "${INFOPLIST_FILE}" --configuration "${CONFIGURATION}" --environment $* > "./.__environment.log"

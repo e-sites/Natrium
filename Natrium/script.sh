70b6afd9073c730af970d363c53476e0e9ac77e6
Bas van Kuijck <bvkuijck@e-sites.nl>
2016-09-28 15:48:46 +0200

    bash script

--- Natrium/script.sh
+++ Natrium/script.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # Shell script to forward xcode build settings to the ruby script
-export PATH
-RUBY_PATH=~/.rvm/bin/ruby-rvm-env
-if [ ! -L $RUBY_PATH ]; then
-  RUBY_PATH=/usr/local/rvm/bin/ruby-rvm-env
-  if [ ! -L $RUBY_PATH ]; then
-    RUBY_PATH=ruby
-  fi
+
+MYPATH="${PROJECT_DIR}/Pods/Natrium/Natrium"
+EXEC="ruby \"${MYPATH}/environmentbuild.rb\" --project_dir \"${PROJECT_DIR}\" --target \"${TARGET_NAME}\" --infoplist_file \"${INFOPLIST_FILE}\" --configuration \"${CONFIGURATION}\" --environment $1 > \"${MYPATH}/.__environment.log\""
+
+if [[ $(ls -A ~/.bashrc) ]]; then
+  EXEC="source ~/.bashrc && $EXEC"
+elif [[ $(ls -A ~/.bash_profile) ]]; then
+  EXEC="source ~/.bash_profile && $EXEC"
+elif [[ $(ls -A ~/.profile) ]]; then
+  EXEC="source ~/.profile && $EXEC"
 fi
-MYPATH="`dirname \"$0\"`"
-VAR="${RUBY_PATH} \"${MYPATH}/environmentbuild.rb\" --project_dir \"${PROJECT_DIR}\" --target \"${TARGET_NAME}\" --infoplist_file \"${INFOPLIST_FILE}\" --configuration \"${CONFIGURATION}\" --environment $1 > \"${MYPATH}/.__environment.log\""
-eval $VAR
+$(bash -l -c  "$EXEC")
